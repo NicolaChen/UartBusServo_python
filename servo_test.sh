c3d4de764bc25d6aa0094865b45d1531140d2908
NicolaChen <chenjl@ops.best>
2022-07-13 11:21:00 +0000

    Add servo_test.sh for PWM servo test

---
+++ servo_test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+cd /sys/class/pwm/pwmchip1
+echo 0 > export
+cd pwm0
+echo 20000000 > period
+echo normal > polarity
+echo "Start servo test, initializing to 180deg."
+echo 1500000 > duty_cycle
+echo 1 > enable
+sleep 1s
+
+echo "Input testing position(deg, 0~360)"
+read tp
+fin=$((500000+$tp*50000/9))
+echo $fin
+sleep 1s
+
+
+echo "Testing..."
+echo $fin > duty_cycle
+
+var=500000
+while(( $var<=2500000 ))
+do
+    echo $var > duty_cycle
+    let var+=100000
+    sleep 4s
+    echo $fin > duty_cycle
+    sleep 5s
+done
+
+cd /sys/class/pwm/pwmchip1
+echo 0 > unexport
+echo "Finish"
